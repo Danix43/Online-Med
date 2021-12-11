998fea06df411b6c8ab88d6c392ff0c0171e485c
Danix43 <daninitu39@gmail.com>
2021-12-11 19:51:12 +0200

    Pacient Homepage done, more fixes needed

--- src/Pages/Pacient/Homepage/Homepage.js
+++ src/Pages/Pacient/Homepage/Homepage.js
@@ -6,29 +6,24 @@ import './Homepage.css'
 
 function Homepage() {
     return (
-        <div>
+        <div className='homepage-container'>
             <Sidebar />
-            <div className='homepage-container'>
 
-                <div className="tip-panel">
-                    <h1 className='tip-title'>Tip</h1>
-                    <p className='tip'>You can book an appointment with a doctor anytime! Check the Appointments tab!</p>
-                </div>
+            <div className="tip-panel">
+                <h1 className='tip-title'>Tip</h1>
+                <p className='tip'>You can book an appointment with a doctor anytime! Check the Appointments tab!</p>
+            </div>
 
-                <table className='reminder-table'>
-                    <tr>
-                        <th>Here are some thins you may want to take a look at!</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <ListItem text='Take a dose of "Aspirine" every day for the 30 - Prescribed by Dr. Smith on 10.12.2019' />
-                        </td>
-                    </tr>
-                </table>
 
-            </div >
+            <ul className='reminder-list'>
+                <li className='reminder-list-header'>Here are some thins you may want to take a look at!</li>
 
-        </div>
+                <li><ListItem text='Take a dose of "Aspirine" every day for the 30 - Prescribed by Dr. Smith on 10.12.2019' /></li>
+                <li><ListItem text='Take a dose of "Aspirine" every day for the 30 - Prescribed by Dr. Smith on 10.12.2019' /></li>
+                <li><ListItem text='Take a dose of "Aspirine" every day for the 30 - Prescribed by Dr. Smith on 10.12.2019' /></li>
+            </ul>
+
+        </div >
 
     )
 }
